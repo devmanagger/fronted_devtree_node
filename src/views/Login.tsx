36d6e8c4369899d65bfba97cfc0ff92668c22643
Dev-Managger <maickcupper@gmail.com>
2025-09-14 06:38:04 +0300

    feat: update Login and Register views with form fields and routing links

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom"
 const Login = () => {
   return (
     <>
-
-        <nav>
-            <Link to='/auth/register'>
+ <h1 className='text-lg text-center text-white block'>Iniciar Sesion</h1>
+        <nav className="mt-10">
+            <Link
+             className="text-center text-white text-lg block"
+            to='/auth/register'>
               No account? Register here.
             </Link>
         </nav>
--- src/views/Register.tsx
+++ src/views/Register.tsx
@@ -2,13 +2,73 @@ import { Link } from 'react-router-dom'
 const Register = () => {
   return (
        <>
-    <div className="">
-        <nav>
-            <Link to='/auth/login'>
+        <h1 className='text-lg text-center text-white block'>Crear Cuenta</h1>
+          <form
+    onSubmit={() => {}}
+    className="bg-white px-5 py-20 rounded-lg space-y-10 mt-10"
+>
+    <div className="grid grid-cols-1 space-y-3">
+        <label htmlFor="name" className="text-2xl text-slate-500">Nombre</label>
+        <input
+            id="name"
+            type="text"
+            placeholder="Tu Nombre"
+            className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+        />
+    </div>
+    <div className="grid grid-cols-1 space-y-3">
+        <label htmlFor="email" className="text-2xl text-slate-500">E-mail</label>
+        <input
+            id="email"
+            type="email"
+            placeholder="Email de Registro"
+            className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+        />
+    </div>
+    <div className="grid grid-cols-1 space-y-3">
+        <label htmlFor="handle" className="text-2xl text-slate-500">Handle</label>
+        <input
+            id="handle"
+            type="text"
+            placeholder="Nombre de usuario: sin espacios"
+            className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+        />
+    </div>
+    <div className="grid grid-cols-1 space-y-3">
+        <label htmlFor="password" className="text-2xl text-slate-500">Password</label>
+        <input
+            id="password"
+            type="password"
+            placeholder="Password de Registro"
+            className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+        />
+    </div>
+
+    <div className="grid grid-cols-1 space-y-3">
+        <label htmlFor="password_confirmation" className="text-2xl text-slate-500">Repetir Password</label>
+        <input
+            id="password"
+            type="password"
+            placeholder="Repetir Password"
+            className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+        />
+    </div>
+
+    <input
+        type="submit"
+        className="bg-cyan-400 p-3 text-lg w-full uppercase text-slate-600 rounded-lg font-bold cursor-pointer"
+        value='Crear Cuenta'
+    />
+</form>
+
+
+    <nav className='mt-10'>
+            <Link
+            className='text-center text-white text-lg block'
+            to='/auth/login'>
                 Already have an account? Login here.
             </Link>
         </nav>
-    </div>
 
     </>
   )
