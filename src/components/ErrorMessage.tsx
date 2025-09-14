479f957350fae0207a4c21e8bb1ca2bbb5281727
Dev-Managger <maickcupper@gmail.com>
2025-09-14 07:30:34 +0300

    feat: implement registration form with validation and error messages

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,9 @@
+type ErrorMessageProps = {
+    children: React.ReactNode;
+}
+
+export const ErrorMessage = ({children}:ErrorMessageProps) => {
+  return (
+    <p className="bg-red-200 rounded-lg text-red-300 p-3 uppercase text-sm font-bold text-center">{children}</p>
+  )
+}
--- src/views/Register.tsx
+++ src/views/Register.tsx
@@ -1,10 +1,29 @@
+import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
+import { ErrorMessage } from '../components/ErrorMessage'
 const Register = () => {
+      const initialValues = {
+        name:'',
+        email:'',
+        handle:'',
+        password:'',
+        password_confirmation:''
+    }
+    const {register,watch,handleSubmit,formState:{errors}}= useForm({
+        defaultValues:initialValues
+    })
+
+
+    console.log(errors)
+    const handleRegister = () => {
+        // Lógica para manejar el registro del usuario
+        console.log('Registrando usuario...');
+      }
   return (
        <>
         <h1 className='text-lg text-center text-white block'>Crear Cuenta</h1>
           <form
-    onSubmit={() => {}}
+    onSubmit={handleSubmit(handleRegister)}
     className="bg-white px-5 py-20 rounded-lg space-y-10 mt-10"
 >
     <div className="grid grid-cols-1 space-y-3">
@@ -14,7 +33,12 @@ const Register = () => {
             type="text"
             placeholder="Tu Nombre"
             className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+            {...register('name',{
+                required:'El Nombre es obligatorio',
+            })}
         />
+
+        {errors.name && <ErrorMessage>{errors.name.message}</ErrorMessage>}
     </div>
     <div className="grid grid-cols-1 space-y-3">
         <label htmlFor="email" className="text-2xl text-slate-500">E-mail</label>
@@ -23,7 +47,11 @@ const Register = () => {
             type="email"
             placeholder="Email de Registro"
             className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+         {...register('email',{
+                required:'El Email es obligatorio',
+            })}
         />
+        {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
     </div>
     <div className="grid grid-cols-1 space-y-3">
         <label htmlFor="handle" className="text-2xl text-slate-500">Handle</label>
@@ -32,7 +60,11 @@ const Register = () => {
             type="text"
             placeholder="Nombre de usuario: sin espacios"
             className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+         {...register('handle',{
+                required:'El Handle es obligatorio',
+            })}
         />
+        {errors.handle && <ErrorMessage>{errors.handle.message}</ErrorMessage>}
     </div>
     <div className="grid grid-cols-1 space-y-3">
         <label htmlFor="password" className="text-2xl text-slate-500">Password</label>
@@ -41,7 +73,12 @@ const Register = () => {
             type="password"
             placeholder="Password de Registro"
             className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+         {...register('password',{
+                required:'El Password es obligatorio',
+            })}
+
         />
+        {errors.password && <ErrorMessage>{errors.password.message}</ErrorMessage>}
     </div>
 
     <div className="grid grid-cols-1 space-y-3">
@@ -51,7 +88,11 @@ const Register = () => {
             type="password"
             placeholder="Repetir Password"
             className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+         {...register('password_confirmation',{
+                required:'El Confirma tu Password',
+            })}
         />
+        {errors.password_confirmation && <ErrorMessage>{errors.password_confirmation.message}</ErrorMessage>}
     </div>
 
     <input
