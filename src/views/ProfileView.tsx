215fb0c2c630b83681640ff0378860e4c9f91e19
Dev-Managger <maickcupper@gmail.com>
2025-09-16 07:06:22 +0300

    feat: implement form handling with validation for ProfileView component

--- src/views/ProfileView.tsx
+++ src/views/ProfileView.tsx
@@ -1,9 +1,17 @@
-
+import {useForm} from 'react-hook-form'
+import { ErrorMessage } from '../components'
 export const ProfileView = () => {
+     const {register,  handleSubmit, formState: {errors}} = useForm({defaultValues:{
+        handle: '',
+        description: '',
+     }})
+        const onSubmit = (data: any) => {
+        console.log(data)
+        }
    return (
         <form
             className="bg-white p-10 rounded-lg space-y-5"
-            onSubmit={() => {}}
+            onSubmit={handleSubmit(onSubmit)}
         >
             <legend className="text-2xl text-slate-800 text-center">Editar Información</legend>
             <div className="grid grid-cols-1 gap-2">
@@ -14,7 +22,9 @@ export const ProfileView = () => {
                     type="text"
                     className="border-none bg-slate-100 rounded-lg p-2"
                     placeholder="handle o Nombre de Usuario"
+                    {...register('handle',{required: 'El handle es obligatorio'})}
                 />
+                {errors.handle && <ErrorMessage>{errors.handle.message}</ErrorMessage>}
             </div>
 
             <div className="grid grid-cols-1 gap-2">
@@ -24,7 +34,9 @@ export const ProfileView = () => {
                 <textarea
                     className="border-none bg-slate-100 rounded-lg p-2"
                     placeholder="Tu Descripción"
+                    {...register('description',{required: 'La descripción es obligatoria'})}
                 />
+                {errors.description && <ErrorMessage>{errors.description.message}</ErrorMessage>}
             </div>
 
             <div className="grid grid-cols-1 gap-2">
