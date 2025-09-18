a939cda1391ba6aefad3eddfab1fb5f98c7749de
Dev-Managger <maickcupper@gmail.com>
2025-09-18 03:25:27 +0300

    feat: refine handleUrlChange function to update state with new link URLs

--- src/views/LinkTreeView.tsx
+++ src/views/LinkTreeView.tsx
@@ -9,12 +9,12 @@ export const LinkTreeView = () => {
     //HandleUrlChange
 
     const handleUrlChange = (e : React.ChangeEvent<HTMLInputElement>) => {
-        console.log(e.target.value)
-        console.log(e.target.name)
+        //ingresa el valor y el nombre del input
+        const updatedLinks = devTreeLinks.map(link => link.name === e.target.name ? {...link, url: e.target.value} : link)
+        console.log(updatedLinks)
+        //actualiza el estado
+        setDevTreeLinks(updatedLinks)
     }
-
-
-
   return (
     <div className="space-y-5">
         {devTreeLinks.map(item =>(
