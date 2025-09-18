a5126e422c66db16a69c7154d9c3dbd1ea10cac1
Dev-Managger <maickcupper@gmail.com>
2025-09-18 03:17:11 +0300

    feat: integrate handleUrlChange prop in DevTreeInpu component and implement URL change handling in LinkTreeView

--- src/components/DevTreeInpu.tsx
+++ src/components/DevTreeInpu.tsx
@@ -4,9 +4,11 @@ import { classNames } from '../utils'
 
 type DevTreeInputProps = {
     item: DevTreeLinks
+    handleUrlChange: (e : React.ChangeEvent<HTMLInputElement>) => void
+
 }
 
-export const DevTreeInpu = ({item}:DevTreeInputProps) => {
+export const DevTreeInpu = ({item , handleUrlChange}:DevTreeInputProps) => {
 
   return (
     <div className="bg-white shadow-sm p-5 flex items-center gap-3 border border-gray-200 rounded-3xl">
@@ -15,11 +17,19 @@ export const DevTreeInpu = ({item}:DevTreeInputProps) => {
         className="w-12 h-12 bg-cover">
 
         </div>
-        <input type="text" className=" flex-1 border border-gray-100 rounded-3xl" />
+        <input
+        type="text"
+        className=" flex-1 border border-gray-100 rounded-3xl"
+        name={item.name}
+        placeholder={`Enter your ${item.name} URL`}
+        value={item.url}
+        onChange={handleUrlChange}
+
+        />
           <Switch
-      checked={item.enabled}
-      onChange={() => {}}
-      className={classNames(
+          checked={item.enabled}
+          onChange={() => {}}
+           className={classNames(
           item.enabled ? 'bg-blue-500' : 'bg-gray-200',
           'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
       )}
--- src/views/LinkTreeView.tsx
+++ src/views/LinkTreeView.tsx
@@ -6,13 +6,19 @@ export const LinkTreeView = () => {
     //State for social links
     const [devTreeLinks, setDevTreeLinks]= useState(social)
 
+    //HandleUrlChange
+
+    const handleUrlChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.value)
+        console.log(e.target.name)
+    }
 
 
 
   return (
     <div className="space-y-5">
         {devTreeLinks.map(item =>(
-            <DevTreeInpu key={item.name} item={item} />
+            <DevTreeInpu key={item.name} item={item} handleUrlChange ={handleUrlChange} />
         ))}
     </div>
   )
