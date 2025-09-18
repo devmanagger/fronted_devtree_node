e3fe8d2daa30ac24418d188c4610917828c3889b
Dev-Managger <maickcupper@gmail.com>
2025-09-18 03:01:34 +0300

    feat: add classNames utility function and integrate Switch component in DevTreeInpu

--- src/components/DevTreeInpu.tsx
+++ src/components/DevTreeInpu.tsx
@@ -1,4 +1,6 @@
+import {Switch} from '@headlessui/react'
 import type { DevTreeLinks } from "../types"
+import { classNames } from '../utils'
 
 type DevTreeInputProps = {
     item: DevTreeLinks
@@ -7,13 +9,29 @@ type DevTreeInputProps = {
 export const DevTreeInpu = ({item}:DevTreeInputProps) => {
 
   return (
-    <div className="bg-white shadow-sm p-5 flex items-center gap-3">
+    <div className="bg-white shadow-sm p-5 flex items-center gap-3 border border-gray-200 rounded-3xl">
         <div
         style={{backgroundImage: `url(/social/icon_${item.name}.svg)`}}
         className="w-12 h-12 bg-cover">
 
         </div>
-        <input type="text" className=" flex-1 border border-gray-100 rounded-xl" />
+        <input type="text" className=" flex-1 border border-gray-100 rounded-3xl" />
+          <Switch
+      checked={item.enabled}
+      onChange={() => {}}
+      className={classNames(
+          item.enabled ? 'bg-blue-500' : 'bg-gray-200',
+          'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
+      )}
+  >
+      <span
+          aria-hidden="true"
+          className={classNames(
+              item.enabled ? 'translate-x-5' : 'translate-x-0',
+              'pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out'
+          )}
+      />
+  </Switch>
     </div>
   )
 }
