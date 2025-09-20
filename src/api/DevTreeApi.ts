1a5590862adc24286d988d046a34a42dc6e48c89
Dev-Managger <maickcupper@gmail.com>
2025-09-21 02:31:19 +0300

    feat: enhance error handling in API functions and update HandleView to utilize getUserByHandle

--- src/api/DevTreeApi.ts
+++ src/api/DevTreeApi.ts
@@ -1,38 +1,52 @@
-import { isAxiosError } from "axios"
-import { axiosClients } from "../config"
-import type {  User } from "../types"
+import { isAxiosError } from "axios";
+import { axiosClients } from "../config";
+import type { User } from "../types";
 
-
- export  const getUser = async () => {
-        try {
-             const {data} = await axiosClients<User>('/users',)
-             return data
-        } catch (error) {
-            if(isAxiosError(error) && error.response){
+export const getUser = async () => {
+    try {
+        const { data } = await axiosClients<User>("/users");
+        return data;
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
             throw new Error(error.response.data.message);
         }
     }
-}
- export  const updateUser = async (formData:User) => {
-        try {
-             const {data} = await axiosClients.patch<String>('/users',formData)
-             return data
-        } catch (error) {
-            if(isAxiosError(error) && error.response){
-            throw new Error(error.response.data.message);
-
-        }
-    }
-}
- export  const uploadImage = async (file:File) => {
-    const formData = new FormData()
-    formData.append('file', file)
-        try {
-         const { data: { image } }: { data: { image: string } } = await axiosClients.post('/users/image', formData);
-        return image
-        } catch (error) {
-            if(isAxiosError(error) && error.response){
+};
+export const updateUser = async (formData: User) => {
+    try {
+        const { data } = await axiosClients.patch<String>("/users", formData);
+        return data;
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
             throw new Error(error.response.data.message);
         }
     }
-}
+};
+export const uploadImage = async (file: File) => {
+    const formData = new FormData();
+    formData.append("file", file);
+    try {
+        const {
+            data: { image },
+        }: { data: { image: string } } = await axiosClients.post(
+            "/users/image",
+            formData
+        );
+        return image;
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.message);
+        }
+    }
+};
+export const getUserByHandle = async (handle: string) => {
+    try {
+        const url = `/${handle}`;
+        const { data } = await axiosClients(url);
+        return data;
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error);
+        }
+    }
+};
--- src/views/HandleView.tsx
+++ src/views/HandleView.tsx
@@ -1,3 +1,19 @@
+import { useParams } from "react-router-dom";
+import { getUserByHandle } from "../api";
+import { useQuery } from "@tanstack/react-query";
+
 export const HandleView = () => {
+    const params = useParams();
+    const handle = params.handle!;
+    const { data, error, isLoading } = useQuery({
+        queryKey: ["handle", handle],
+        queryFn: () => getUserByHandle(handle),
+        retry: 1,
+    });
+
+    console.log(isLoading);
+    console.log(error);
+    console.log(data);
+
     return <div>HandleView</div>;
 };
