3f1af2a4374646b26ab3c17227b40570c2a1d892
Dev-Managger <maickcupper@gmail.com>
2025-09-21 03:38:06 +0300

    feat: enhance HandleData component to parse and display enabled social network links

--- src/components/HandleData.tsx
+++ src/components/HandleData.tsx
@@ -1,10 +1,13 @@
-import type { UserHandle } from "../types";
+import type { SocialNetwork, UserHandle } from "../types";
 
 type HandleProps = {
     data: UserHandle;
 };
 
 export const HandleData = ({ data }: HandleProps) => {
+    const links: SocialNetwork[] = JSON.parse(data.links).filter(
+        (link: SocialNetwork) => link.enabled
+    );
     return (
         <div className="space-y-6 text-white">
             <p className="text-5xl text-center font-black">{data.handle}</p>
@@ -16,6 +19,33 @@ export const HandleData = ({ data }: HandleProps) => {
                 />
             )}
             <p className="text-lg text-center font-bold">{data.description}</p>
+            <div className="mt-20 fle flex-col ga-6">
+                {links.length ? (
+                    links.map((link) => (
+                        <a
+                            href={link.url}
+                            target="_blank"
+                            rel="noreferrer noopener"
+                            className="bg-white mt-3 px-5 py-2 flex items-center gap-5 rounded-3xl"
+                            key={link.id}
+                        >
+                            <img
+                                src={`/social/icon_${link.name}.svg`}
+                                alt="red_social"
+                                className="w-12"
+                            />
+                            <p className="text-gray-400 capitalize text-lg">
+                                Visita mi:{" "}
+                                <strong className="text-black">
+                                    {link.name}
+                                </strong>
+                            </p>
+                        </a>
+                    ))
+                ) : (
+                    <p className="text-center">No hay enlace en este perfil</p>
+                )}
+            </div>
         </div>
     );
 };
